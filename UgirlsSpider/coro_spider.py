c3d5b51f31736c1c3534c1d59efe810c90c66b61
ruxtain <201220030319@hnu.edu.cn>
2018-08-18 10:00:28 +0800

    use coroutines to download hot chicks

---
+++ UgirlsSpider/coro_spider.py
@@ -0,0 +1,41 @@
+#! /Users/michael/anaconda3/bin/python
+# @Date:   2018-08-18 09:34:49
+# 协程下载
+
+from urllib.parse import urljoin
+from bs4 import BeautifulSoup
+import asyncio
+import aiohttp
+import os
+
+base_url = 'https://www.ugirls.com/Shop/Detail/Product-{:03}.html'
+
+root = 'coro-images'
+os.makedirs(root, exist_ok=True)
+loop = asyncio.get_event_loop()
+
+
+async def get_page(url):
+    async with aiohttp.ClientSession() as session:
+        async with session.get(url) as response:
+            print('获取页面', url)
+            text = await response.text()
+            soup = BeautifulSoup(text, 'lxml')
+            imgs = soup.select('div.chao div.yang > img.scaleimg')
+            for img in imgs:
+                image_url = urljoin(base_url, img['src'])
+                await get_image(image_url)
+
+
+async def get_image(image_url):
+    async with aiohttp.ClientSession() as session:
+        async with session.get(image_url) as response:
+            content = await response.read()
+            name = os.path.basename(image_url)
+            with open(os.path.join(root, name), 'wb') as f:
+                f.write(content)
+                print('下载图片', image_url)
+
+
+tasks = [get_page(base_url.format(i)) for i in range(1, 21)]
+loop.run_until_complete(asyncio.wait(tasks))
